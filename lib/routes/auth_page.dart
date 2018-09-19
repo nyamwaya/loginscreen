ab684bde1f3ea385fa52e8c8150e456ecd78018e
nyamwaya <alecksonnyamwaya@gmail.com>
2018-09-19 17:27:16 -0500

    added confirm password

--- lib/routes/auth_page.dart
+++ lib/routes/auth_page.dart
@@ -8,7 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_auth_world/firebase_helper/database.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AuthPage extends StatefulWidget {
   AuthPage({this.databse, this.auth, this.onSignedIn});
@@ -24,6 +24,13 @@ enum FormType { login, register }
 
 class _AuthPageState extends State<AuthPage> {
   final formKey = new GlobalKey<FormState>();
+  // This is not the best way to show a snack bar.
+  // A more efficient solution is to split your build function into several widgets. This introduces a
+  // new context from which you can obtain the Scaffold. In this solution, you would have an outer widget
+  // that creates the Scaffold populated by instances of your new inner widgets, and then in these inner
+  // widgets you would use Scaffold.of().
+  // Please refactor...I did this to save time
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   String _email;
   String _password;
@@ -31,13 +38,11 @@ class _AuthPageState extends State<AuthPage> {
   String _fName;
   String _lName;
 
-
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
-
 
   FormType _formType = FormType.login;
 
@@ -61,20 +66,22 @@ class _AuthPageState extends State<AuthPage> {
           print("Signed in: $userId");
         } else {
           //TODO: save name and info here??
-          String userId = await widget.auth.createUserWithEmailAndPassword(_email, _password);
+          String userId = await widget.auth
+              .createUserWithEmailAndPassword(_email, _password);
           print("Registered user: $userId ");
 
           await widget.databse.pushProfileToDatabase(userId, _fName, _email);
-
-         
-
-        
         }
         widget.onSignedIn();
       } catch (e) {
         print("Error: $e");
       }
     }
+  }
+
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(value)));
   }
 
   void moveToRegister() {
@@ -91,80 +98,78 @@ class _AuthPageState extends State<AuthPage> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        key: _scaffoldKey,
         body: SafeArea(
-      child: ListView(
-        shrinkWrap: true,
-        reverse: false,
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        children: <Widget>[
-          SizedBox(height: 100.0),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
+            shrinkWrap: true,
+            reverse: false,
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
             children: <Widget>[
-              Image.asset(
-                'assets/images/logo.png',
-                fit: BoxFit.scaleDown,),
+              SizedBox(height: 100.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/logo.png',
+                    fit: BoxFit.scaleDown,
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Kwizny',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontFamily: 'Quicksand',
+                      fontSize: 32.0,
+                    ),
+                  ),
+                ],
+              ),
               SizedBox(height: 16.0),
-              Text(
-                'Kwizny',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontFamily: 'Quicksand',
-                  fontSize: 32.0,
+              new Container(
+                padding: EdgeInsets.all(16.0),
+                child: new Form(
+                  key: formKey,
+                  child: new Column(
+                    children: buildUserInput() +
+                        //buildForgotPassword() +
+                        buildLoginButton() +
+                        // buildSeparator() +
+                        // buildGoogleButton() +
+                        buildRegister(),
+                  ),
                 ),
               ),
+
+              //TODO: Add or sign in with google
             ],
           ),
-          SizedBox(height: 16.0),
-          new Container(
-            padding: EdgeInsets.all(16.0),
-            child: new Form(
-              key: formKey,
-              child: new Column(
-                children: buildUserInput() +
-                    //buildForgotPassword() +
-                    buildLoginButton() +
-                    // buildSeparator() +
-                    // buildGoogleButton() +
-                    buildRegister(),
-              ),
-            ),
-          ),
-
-          //TODO: Add or sign in with google
-        ],
-      ),
-    ));
+        ));
   }
 
   List<Widget> buildForgotPassword() {
-    
-      return [
-        SizedBox(height: 8.0),
+    return [
+      SizedBox(height: 8.0),
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FlatButton(
-              child: Text("Forgot Password?",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black54,
-                    fontSize: 15.0,
-                    fontFamily: 'Quicksand',
-                  )),
-              onPressed: () => {},
-            )
-          ],
-        )
-        //SizedBox(height: 8.0),
-      ];
-    
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FlatButton(
+            child: Text("Forgot Password?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                  fontSize: 15.0,
+                  fontFamily: 'Quicksand',
+                )),
+            onPressed: () => {},
+          )
+        ],
+      )
+      //SizedBox(height: 8.0),
+    ];
   }
 
   List<Widget> buildRegister() {
@@ -325,7 +330,8 @@ class _AuthPageState extends State<AuthPage> {
           child: TextFormField(
             controller: _firstNameController,
             decoration: InputDecoration(labelText: "first name"),
-            validator: (value) => value.isEmpty ? 'Last name can\'t be empty' : null,
+            validator: (value) =>
+                value.isEmpty ? 'Last name can\'t be empty' : null,
             onSaved: (value) => _fName = value,
           ),
         ),
@@ -360,18 +366,18 @@ class _AuthPageState extends State<AuthPage> {
             obscureText: true,
           ),
         ),
-        // TODO: uncoment once you get the update username figured out
-        //PrimaryColorOverride(
-        //   color: kShrineBrown900,
-        //   child: new TextFormField(
-        //     controller: _confirmPasswordController,
-        //     decoration: new InputDecoration(labelText: "confirm password"),
-        //     validator: (value) =>
-        //         value.isEmpty ? 'Confirm Password can\'t be empty' : null,
-        //     onSaved: (value) => _confirmPassword = value,
-        //     obscureText: true,
-        //   ),
-        // )
+
+        PrimaryColorOverride(
+          color: kShrineBrown900,
+          child: new TextFormField(
+            controller: _confirmPasswordController,
+            decoration: new InputDecoration(labelText: "confirm password"),
+            validator: (value) =>
+                value.isEmpty ? 'Confirm Password can\'t be empty' : null,
+            onSaved: (value) => _confirmPassword = value,
+            obscureText: true,
+          ),
+        )
       ];
     }
   }
@@ -405,24 +411,40 @@ class _AuthPageState extends State<AuthPage> {
         //SizedBox(height: 8.0),
         ButtonBar(
           children: <Widget>[
-                RaisedButton(
-          color: Colors.redAccent,
-          child: Text(
-            "Next",
-            style: TextStyle(
-                fontFamily: 'Roboto',
-                color: Colors.white,
-                fontWeight: FontWeight.bold),
-          ),
-          elevation: 8.0,
-          shape: BeveledRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(7.0))),
-          onPressed: validateAndSubmit,
-        )
-          ],
+            RaisedButton(
+              color: Colors.redAccent,
+              child: Text(
+                "Next",
+                style: TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              elevation: 8.0,
+              shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(7.0))),
+              //onPressed: validateAndSubmit,
+              onPressed: () {
+                if (_passwordController.value.text.toString().trim() ==
+                    _confirmPasswordController.value.text.toString().trim()) {
+                  //Passwords match
+                  validateAndSubmit();
+                } else {
+                  //Passwords dont match
+                  showInSnackBar(
+                      "Your Password and confirm password did not match, try again");
 
+                  String pWord =
+                      _passwordController.value.text.toString().trim();
+                  String conPWord =
+                      _confirmPasswordController.value.text.toString().trim();
+                  print(
+                      "Your Password and confirm password do not match: $pWord  $conPWord");
+                }
+              },
+            )
+          ],
         )
-    
       ];
     }
   }
