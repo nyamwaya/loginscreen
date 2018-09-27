6ccfd136aae5586eaea145843ba37ba866c4eb3f
nyamwaya <alecksonnyamwaya@gmail.com>
2018-09-27 16:04:08 -0500

    re-arrange to column

--- lib/routes/profile_page.dart
+++ lib/routes/profile_page.dart
@@ -11,12 +11,16 @@ class _ProfilePage extends State<ProfilePage> {
     return Scaffold(
       body: Container(
         constraints: new BoxConstraints.expand(),
-        child: new ListView(
-          padding: const EdgeInsets.all(1.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //padding: const EdgeInsets.all(1.0),
           children: <Widget>[
-            _getToolbar(context),
+           // _getToolbar(context),
             _buildHeader(),
-            _buildStatsBar(),
+            SizedBox(
+              height: 30.0,
+            ),
+            //_buildStatsBar(),
             _buildOptions()
           ],
         ),
@@ -38,9 +42,9 @@ class _ProfilePage extends State<ProfilePage> {
 
   Container _buildHeader() {
     return new Container(
-      padding: const EdgeInsets.only(left: 16.0, top: 20.0, bottom: 24.0),
+      padding: const EdgeInsets.only(left: 16.0,  bottom: 24.0),
       //height: 218.0,
-      child: new Row(
+      child: new Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           new Container(
@@ -52,6 +56,7 @@ class _ProfilePage extends State<ProfilePage> {
                     new BoxShadow(color: Colors.black38, blurRadius: 32.0)
                   ],
                   image: new DecorationImage(
+                    //TODO: if not profile photo, add random robot 
                     image: NetworkImage(
                         'https://images.unsplash.com/photo-1531078215167-91fcfe45b39e?ixlib=rb-0.3.5&s=786d61b2a3ed74d1f981ee230b1a9457&auto=format&fit=crop&w=1098&q=80.jpg'),
                     fit: BoxFit.cover,
@@ -59,10 +64,10 @@ class _ProfilePage extends State<ProfilePage> {
                   borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
                   border: new Border.all(color: Colors.white, width: 4.0))),
           new Padding(
-            padding: const EdgeInsets.only(left: 15.0),
+            padding: const EdgeInsets.only(left: 15.0, top: 15.0),
             child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new Text(
                   "Kasha Killingsworth",
